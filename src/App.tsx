20fd144b029fb45bcf1bcf683c37f8f9fcb4e372
Sergey Goncharov <sergon165@icloud.com>
2024-07-28 12:43:49 +0500

    add lazy loading routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import React, { useEffect, useRef } from "react";
+import React, { Suspense, useEffect, useRef } from "react";
+import Loadable from "react-loadable";
 import "./scss/app.scss";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Root from "./pages/Root";
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
+// import Home from "./pages/Home";
+// import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
-import PizzaDetail from "./pages/PizzaDetail";
+// import PizzaDetail from "./pages/PizzaDetail";
 import { useSelector } from "react-redux";
 import { RootState, useAppDispatch } from "./redux/store";
 import { LocalStorage } from "./utils/LocalStorage";
 import { fetchPizzasInfo } from "./redux/cartSlice";
+
+// const Home = React.lazy(
+//   () => import(/* webpackChunkName: "Home" */ "./pages/Home")
+// );
+const LoadableHome = Loadable({
+  loader: () => import(/* webpackChunkName: "Home" */ "./pages/Home"),
+  loading: () => <></>,
+});
+const Cart = React.lazy(
+  () => import(/* webpackChunkName: "Cart" */ "./pages/Cart")
+);
+const PizzaDetail = React.lazy(
+  () => import(/* webpackChunkName: "PizzaDetail" */ "./pages/PizzaDetail")
+);
 
 const App: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -41,15 +56,23 @@ const App: React.FC = () => {
       children: [
         {
           path: "",
-          element: <Home />,
+          element: <LoadableHome />,
         },
         {
           path: "pizza/:id",
-          element: <PizzaDetail />,
+          element: (
+            <Suspense>
+              <PizzaDetail />
+            </Suspense>
+          ),
         },
         {
           path: "cart",
-          element: <Cart />,
+          element: (
+            <Suspense>
+              <Cart />
+            </Suspense>
+          ),
         },
       ],
     },
